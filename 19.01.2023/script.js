ca542430394670778c9caec3487ee549ba259bc8
Max <Nikolaev.max05@gmail.com>
2023-01-26 11:54:11 +0300

    new

--- 19.01.2023/script.js
+++ 19.01.2023/script.js
@@ -3,6 +3,30 @@ function clck(){
     console.log(`Click ${++n} times`   )
 }
 
-const getNumber = function(a, b){
+const getNumber = function(a = 101, b = 0) //math.floor не включает последнее число
+{
     return Math.floor(Math.random() * (a-b) + b)
+}
+let w
+function show(){
+    // window.open("https://wikipedia.org", "");
+   w = window.open("","","width=300, height=300, top=200, left=200");
+}
+function exit(){
+    if(w){
+        w.close();
+    }
+}
+
+function changeSize(){
+    let width = getNumber(screen.width - 200, 100)
+    let height = getNumber(screen.height - 200, 100)
+    if(w){
+        w.resizeTo(width, height)
+    }
+}
+
+function changePlace(){
+    let x = (screen.width - w.innerWidth)
+    w.moveTo()
 }
