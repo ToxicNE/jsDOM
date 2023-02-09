eda95e2ff6481b2bd4a07036142c584d92d43389
Max <93477850+Max-IThub@users.noreply.github.com>
2023-02-09 13:52:24 +0300

    ds
    dsda

--- functions.js
+++ functions.js
@@ -1,7 +1,21 @@
-const getNumber = function(a = 101, b = 0){
-    return Math.floor(Math.random() * (a-b) + b)
+function getNumber(max = 256, min = 0){
+    return Math.floor(Math.random() * (max-min) + min)
 }
-const getColor = function(){
-    return `rgba(${getNumber(256)},
-    ${getNumber(256)},${getNumber(256)}, ${a / 100})`
+function getColor(a = 100){
+    return `rgba(${getNumber()},
+    ${getNumber()},${getNumber()}, ${a / 100})`
+}
+function getGradient(cnt = 2, type = "linear", dir){
+    let str = ""
+    switch(type){
+        case "linear": str += `linear-gradient(`; break;
+        case "radial": str += `radial-gradient(`; break;
+    }
+    if(dir){
+        str += dir + ","
+    }
+    while(cnt--){
+        str += getColor(getNumber(101));
+        str += cnt ? "," : ")";
+    }
 }
