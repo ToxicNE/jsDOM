53ec54b602066356f9d67684fdba3a37cdcf586e
Max-IThub <Nikolaev.max05@gmail.com>
2023-04-02 09:36:36 +0300

    new commit

---
+++ kt4/srcipt.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector('.slider');
+const thumb = slider.querySelector('.thumb');
+const range = slider.querySelector('.range');
+
+let isDragging = false;
+
+thumb.addEventListener('mousedown', (event) => {
+  isDragging = true;
+});
+
+document.addEventListener('mousemove', (event) => {
+  if (isDragging) {
+    let thumbPosition = event.clientX - slider.getBoundingClientRect().left;
+    let rangeWidth = range.offsetWidth;
+    let thumbWidth = thumb.offsetWidth;
+    
+    if (thumbPosition < 0) {
+      thumbPosition = 0;
+    } else if (thumbPosition > rangeWidth - thumbWidth) {
+      thumbPosition = rangeWidth - thumbWidth;
+    }
+    
+    thumb.style.left = thumbPosition + 'px';
+  }
+});
+
+document.addEventListener('mouseup', (event) => {
+  isDragging = false;
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const slider = document.querySelector('.slider');
+  const thumb = slider.querySelector('.thumb');
+  // остальной код для слайдера
+});
+
